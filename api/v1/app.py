06cfb4ff6a0024bfaec3f552514d304e7f31f4bd
Akos-1 <veronicagyasi@gmail.com>
2023-12-03 13:44:14 -0800

    fixed errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-import os
+
+import os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -21,6 +24,7 @@ def teardown_appcontext(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
