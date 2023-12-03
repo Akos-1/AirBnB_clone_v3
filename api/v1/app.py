412f82b22d2cf56fcd724f2ee4fe66e97e41ee85
Francaicey <francawhyte447@gmail.com>
2023-12-03 19:00:00 +0000

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""A variable app"""
-
-from os import getenv
-from flask import Flask, jsonify
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Removes object after each request"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handles errors"""
-    return jsonify({"error": "Not found"}), 404
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
