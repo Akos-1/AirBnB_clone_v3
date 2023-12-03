70d7bbbbe151d2a9898d89f2d03b88a008396037
Francaicey <francawhyte447@gmail.com>
2023-12-03 20:10:21 +0000

    updated files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""create a variable app_views which is an instance of Blueprint"""
-
+"""Initialize Blueprint views"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.amenities import *
