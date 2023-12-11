8d26ddf42854cc7169c02e085b0a9e1896284deb
Akos-1 <veronicagyasi@gmail.com>
2023-12-10 21:12:43 -0800

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
-
+"""A route is created for object"""
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route('/status', methods=['GET'])
 
+def api_status():
+    """a JSON response for RESTful API health is returned"""
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
+    response = {'status': 'OK'}
+    return jsonify(response)
 
+@app_views.route('/stats', methods=['GET'])
 
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+def get_stats():
+    """the number of each objects by type is retrieved"""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
